27fd915c7d4f3e00a3f48f229c9bbf595fdaf669
Dany <daniilakimov@bk.ru>
2021-11-02 15:04:45 +0300

    new task

--- src/Collection/main.java
+++ src/Collection/main.java
@@ -1,20 +1,49 @@
 package Collection;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class main {
-    public static void main (String[] args){
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
 
+    private static void task1() {
+        Map<String, Integer> hm = new HashMap<>();
+        String[] words = {
+                "Coffee", "Potato", "Alpha",
+                "Cheese", "Beta", "Humster",
+                "Dog", "Cat", "Java",
+                "Yava", "Kent", "Coffee",
+                "Dog", "Beta", "Humster",
+                "Cat", "Java", "Yava", "Dog"
+        };
+
+        for (int i = 0; i < words.length; i++) {
+            if (hm.containsKey(words[i]))
+                hm.put(words[i], hm.get(words[i]) + 1);
+            else
+                hm.put(words[i], 1);
+        }
+        System.out.println(hm);
+    }
+
+
+    private static void task2(){
         PhoneBook hm = new PhoneBook();
         hm.add("Иванов", "+79256745674");
         hm.add("Алексеев", "+79264568361");
         hm.add("Катеров", "+79255467363");
         hm.add("Никаноров", "+79268328443");
         hm.add("Елкин", "+79258674629");
-
-        System.out.println(hm);
-        System.out.println(hm.get("Иванов"));
+        System.out.println(hm.get("+79258674629"));
+        System.out.println(hm.get("Катеров"));
 
 
     }
 }
+
+
+
